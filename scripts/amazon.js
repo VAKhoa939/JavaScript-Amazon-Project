3db7c1d200ddaf45ca3db4678b0fb5284428b20a
VAKhoa939 <21110046@student.hcmute.edu.vn>
2024-09-22 23:14:15 +0700

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -55,7 +55,7 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelector('.js-add-to-cart').forEach(button => {
+document.querySelectorAll('.js-add-to-cart').forEach(button => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
@@ -74,5 +74,13 @@ document.querySelector('.js-add-to-cart').forEach(button => {
         quantity: 1
       });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach(item => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').textContent = cartQuantity;
   });
 });
